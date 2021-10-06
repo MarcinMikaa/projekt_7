e5b4def3662238f6abb3113364cfaa67cbc5809d
Marcin <marcin.mika123@gmail.com>
2021-10-06 11:58:49 +0200

    Final

--- src/Users.js
+++ src/Users.js
@@ -4,14 +4,37 @@ function Users() {
   const [users, setUsers] = useState([
     { id: 1, name: "John Doe", email: "john.doe@gmail.com" },
     { id: 2, name: "Jane Doe", email: "jane.doe@gmail.com" },
-    { id: 3, name: "Marin Mika", email: "marcin.mika@gmail.com" },
+    { id: 3, name: "Marcin Mika", email: "marcin.mika@gmail.com" },
   ]);
+
+  const [newName, setName] = useState("");
+  const [newEmail, setEmail] = useState("");
+
+  const getName = (event) => {
+    setName(event.target.value);
+  };
+
+  const getEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const sendInputs = (event) => {
+    event.preventDefault();
+    console.log(newName);
+    console.log(newEmail);
+    const newUser = {
+      id: users.length + 1,
+      name: newName,
+      email: newEmail,
+    };
+    setUsers([...users, newUser]);
+  };
 
   return (
     <div>
-      <form>
-        <input type="text"></input>
-        <input type="text"></input>
+      <form onSubmit={sendInputs}>
+        <input type="text" onChange={getName}></input>
+        <input type="text" onChange={getEmail}></input>
         <button>ADD</button>
       </form>
 
